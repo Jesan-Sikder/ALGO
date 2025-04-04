a06adf0605fcc9d7b641fcbcd18c221d08fb413b
Jesan Sikder <jesanalpha545@gmail.com>
2025-04-04 13:13:21 +0600

    testing

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,34 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ass return 0
-#define ll long long
-#define dd double
-int main(){
-int t,n;
-cin>>t;
-for (int kalu = 1; kalu <= t; kalu++)
-{
-    cin>>n;
-    int j=n;
-    char arr[n],count=0;
-    for (int i =0; i < n; i++)
-    {
-        cin>>arr[i];
-        if(arr[i]=='#'){
-            count++;
-        }
-    }
-    int num = j-count;
-    //cout<<num<<endl;
-    int total=num/2;
-    cout<<"case "<<kalu<<": "<<total<<endl;
-
-}
-
-
-
-
-
-
-ass;
-}
+if(p[i]!=2){
+//     flag = 0;
+//    }
